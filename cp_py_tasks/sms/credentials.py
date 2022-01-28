113ab8cef1a6bf04ccda8894cf4e6d4d70195834
anujmittal94 <anujanilmittal@gmail.com>
2022-01-28 15:29:05 +0530

    intial commit

--- cp_py_tasks/sms/credentials.py
+++ cp_py_tasks/sms/credentials.py
@@ -1,4 +1,4 @@
 account_sid = 'ACe498088db5c60969b1e8f1aca33940f4'
-auth_token = '8921c81b7267241c22fe5b508c5a3c68'
+auth_token = '405fc98f845ae984e626d14edef76f00'
 my_cell = '+91 83692 59146'
 my_twi = '+1 740 573 7497'
